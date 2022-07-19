0c9c952c1f513057d7aa19077597473cf6b856c9
Vitor Campos <vitorcampos742@gmail.com>
2022-07-19 16:20:41 -0300

    testes do endpoint /login

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,52 +1,58 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
-// import User from '../database/models/User'
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import User from '../database/models/User'
 
-// import { app } from '../app';
-// import Example from '../database/models/ExampleModel';
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
 
-// import { Response } from 'superagent';
+import { Response } from 'superagent';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// const user = {
-//   username: 'fulano',
-//   role: 'helper',
-//   email: 'fulano@gmail.com',
-//   password: '123456'
-// } as User;
+const user = {
+  username: 'user',
+  role: 'user',
+  email: 'user@user.com',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+} as User;
 
-// describe('Seu teste', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
+describe('Teste o endpoint de login', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
 
-//   let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-//   before(async () => {
-//     sinon
-//       .stub(User, "findOne")
-//       .resolves(user);
-//   });
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(user);
+  });
 
-//   after(()=>{
-//     (Example.findOne as sinon.SinonStub).restore();
-//   })
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
 
-//   it('...', async () => {
-//     chaiHttpResponse = await chai
-//        .request(app)
-//        .post('/login')
-//        .send({ email: 'admin@admin.com', password: 'secret_admin' });
+  it('testa se as propriedades corretas sao retornadas', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'user@user.com', password: 'secret_user' });
 
-//     expect(...)
-//   });
+    expect(chaiHttpResponse.body).to.have.property('token')
+    expect(chaiHttpResponse.body.token).be.a('string')
+  });
 
-//   it('Seu sub-teste', () => {
-//     expect(false).to.be.eq(true);
-//   });
-// });
+  it('testa o status do retorno', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'user@user.com', password: 'secret_user' });
+
+    expect(chaiHttpResponse).to.have.status(200)
+  });
+});
